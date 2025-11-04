8b01d49fbbabd933af94bea3107f8b6f106043fa
Harshcreator <jayson22603@gmail.com>
2025-11-04 19:04:11 +0530

    nqueens and fixed cide

--- DAA Python/3_fractional_knapsack.py
+++ DAA Python/3_fractional_knapsack.py
@@ -1,22 +1,26 @@
 def fractional_knapsack():
-    weights=[10,20,30]
-    values=[60,100,120]
-    capacity=50
-    res=0
-    for pair in sorted(zip(weights,values), key= lambda x: x[1]/x[0], reverse=True):
-        if capacity<=0: 
-            break 
-        if pair[0]>capacity:
-            res+=int(capacity * (pair[1]/pair[0])) 
-            capacity=0
-        elif pair[0]<=capacity:
-            res+=pair[1]
-            capacity-=pair[0]
-    print(res)        
+    weights = [10, 20, 30]
+    values = [60, 100, 120]
+    capacity = 50
+    res = 0
 
-if __name__=="__main__":
+    # Create list of (weight, value, ratio) and sort by ratio descending
+    items = sorted(
+        [(w, v, v / w) for w, v in zip(weights, values)],
+        key=lambda x: x[2],
+        reverse=True
+    )
+
+    for w, v, ratio in items:
+        if capacity <= 0:
+            break
+        if w > capacity:
+            res += capacity * ratio
+            capacity = 0
+        else:
+            res += v
+            capacity -= w
+    print(int(res))
+
+if __name__ == "__main__":
     fractional_knapsack()
-    
-
-# Output:
-# 240
